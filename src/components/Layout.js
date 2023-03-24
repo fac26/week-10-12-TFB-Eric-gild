99ef675005651a1323aac417d8b54373cc833f93
LauraK0 <laurakeating@live.co.uk>
2023-03-24 17:42:58 +0000

    conditional top nav statement added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,9 +13,7 @@ export default function Layout({ children, home, isBusinessPage }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
-        <TopBar />
-      </header>
+      <header>{!home ? null : <TopBar />}</header>
       <div>
         <main>{children}</main>
         {!home ? <NavBar isBusinessPage={isBusinessPage} /> : null}
