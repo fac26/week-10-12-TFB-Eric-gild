5e97a5817f7584b5c126b4b3769e8ecc38540d8c
space doge <99536044+d0g3bl2@users.noreply.github.com>
2023-03-24 14:28:54 +0000

    add logo and copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,21 @@
 import GreenButton from '@/components/GreenButton';
+import LogoLandingPage from '@/components/LogoLandingPage';
+import style from '@/styles/Home.module.css';
 
 export default function Home() {
   const vendorTitle = 'Vendors';
+  const source = '/logoLandingPage.png';
+  const alt =
+    'The word hope written in white in calligraphy style, with the letter O in the shape of a heart and the legend food. The strap line reads donate with love. ';
 
-  return <GreenButton title={vendorTitle} />;
+  return (
+    <>
+      <GreenButton title={vendorTitle} />
+      <LogoLandingPage src={source} width={300} height={131} alt={alt} />
+      <p className={style.copy}>
+        The stigma-free way of sharing emergency food{' '}
+      </p>
+      <p className={style.readyForFood}>Ready for food?</p>
+    </>
+  );
 }
