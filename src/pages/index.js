0f8e5a8abbc01c5b24750fffc5f6294216ded32b
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-27 11:07:31 +0100

    Git conflict

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,20 @@
-import GreenButton from '@components/GreenButton';
-import LogoLandingPage from '@components/LogoLandingPage';
-import style from '@styles/Home.module.css';
-import Layout from 'components/Layout';
+import GreenButton from "@components/GreenButton";
+import LogoLandingPage from "@components/LogoLandingPage";
+import style from "@styles/Home.module.css";
+import Layout from "components/Layout";
 
 export default function Home() {
-  const vendorTitle = 'Vendors';
-  const source = '/logoLandingPage.png';
+  const vendorTitle = "Vendors";
+  const source = "/logoLandingPage.png";
   const alt =
-    'The word hope written in white in calligraphy style, with the letter O in the shape of a heart and the legend food. The strap line reads donate with love. ';
+    "The word hope written in white in calligraphy style, with the letter O in the shape of a heart and the legend food. The strap line reads donate with love. ";
 
   return (
     <Layout isBusinessPage>
       <GreenButton title={vendorTitle} />
       <LogoLandingPage src={source} width={300} height={131} alt={alt} />
       <p className={style.copy}>
-        The stigma-free way of sharing emergency food{' '}
+        The stigma-free way of sharing emergency food
       </p>
       <p className={style.readyForFood}>Ready for food?</p>
     </Layout>
