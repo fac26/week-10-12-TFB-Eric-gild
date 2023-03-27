04d7f23892ab42374dddb61d2f17e2edd845509b
ighellache <78818760+ighellache@users.noreply.github.com>
2023-03-27 11:50:42 +0100

    changed path for router.push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,35 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { Auth } from '@supabase/auth-ui-react'
-import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { Auth } from "@supabase/auth-ui-react";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const session = useSession()
-	const supabase = useSupabaseClient()
-	const router = useRouter()
+  const session = useSession();
+  const supabase = useSupabaseClient();
+  const router = useRouter();
 
-	useEffect(() => {
-		if (session) {
-			router.push('/VendorUploadFormPage')
-		}
-	}, [session])
+  useEffect(() => {
+    if (session) {
+      router.push("/");
+    }
+  }, [session]);
 
   return (
     <div>
-    {!session ? (
-      <>
-      <h1>HOPE</h1>
-        <Auth
-          supabaseClient={supabase}
-          providers
-        />
-       
-      </>
-    ) : (
-      <></>
-    )}
+      {!session ? (
+        <>
+          <h1>HOPE</h1>
+          <Auth supabaseClient={supabase} providers />
+        </>
+      ) : (
+        <></>
+      )}
     </div>
-  )
+  );
 }
