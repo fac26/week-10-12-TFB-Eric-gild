0b58caf45402231ccbd514a3414e4e4d6059be80
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-27 15:37:21 +0100

    Sign-in / sign-up testing passes

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -1,9 +1,9 @@
-describe('NavBar component', () => {
+context('Navigation', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/find-food');
   });
-  it('should exist on the page', () => {
-    cy.visit('http://localhost:3000/find-food');
+
+  it('Passes', () => {
     cy.get('[data-cy="help"]').should('exist');
     cy.get('[data-cy="search"]').should('exist');
     cy.get('[data-cy="bookmark"]').should('exist');
