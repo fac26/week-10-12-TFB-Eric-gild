a4bacaeb682f4b82bcc2c208ab81e6b09cdcf710
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-28 12:31:11 +0100

    Attempts at styling Supabase Auth component

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -3,6 +3,7 @@ import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import Layout from 'components/Layout';
+import Button from '@components/Button';
 
 export default function SignIn() {
   const session = useSession();
@@ -17,17 +18,27 @@ export default function SignIn() {
 
   return (
     <Layout pageTitle='Sign in'>
-      <Button buttonName={'Sign-In'} buttonLink='/vendor-upload-form' />
       <div>
         {!session ? (
           <>
             <h1>HOPE</h1>
-            <Auth supabaseClient={supabase} providers />
+            <Auth
+              supabaseClient={supabase}
+              providers
+              appearance={{
+                style: {
+                  email: { background: 'red', color: 'white' },
+                  anchor: { color: 'blue' },
+                  //..
+                },
+              }}
+            />
           </>
         ) : (
           <></>
         )}
       </div>
+      <Button buttonName={'Sign In'} buttonLink='/vendor-upload-form' />
     </Layout>
   );
 }
