75f71eb5534d56fd1b35a1228829c4f20ebe7036
ighellache <78818760+ighellache@users.noreply.github.com>
2023-03-29 10:49:44 +0100

    signin fix

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,9 +1,21 @@
 import BackButton from './BackButton';
+import { useRouter } from 'next/router';
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 
 export default function TopBar() {
+  const supabase = useSupabaseClient();
+  const session = useSession();
+  const router = useRouter();
+
   return (
     <nav className='sticky w-11/12 mx-auto border-b flex justify-between items-center h-16 px-4'>
       <BackButton className='flex-none' />
+      <button
+        className={`text-white`}
+        onClick={() => supabase.auth.signOut() && router.push('/')}
+      >
+        Sign Out
+      </button>
       <h3 className='text-right text-accentcolor2 text-2xl font-heading'>
         HOPE
       </h3>
--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import Layout from 'components/Layout';
 import Button from '@components/Button';
+import TopBar from 'components/TopBar';
 
 export default function SignIn() {
   const session = useSession();
@@ -17,48 +18,56 @@ export default function SignIn() {
   }, [session]);
 
   return (
-    <Layout pageTitle='Sign in' isBusinessPage>
-      <div className='flex justify-center font-cursive text-custom'>
-        {!session ? (
-          <>
-            <Auth
-              supabaseClient={supabase}
-              providers
-              appearance={{
-                style: {
-                  button: {
-                    padding: 10,
-                    borderColor: '#540375',
-                    backgroundColor: '#540375',
-                    borderRadius: 7,
-                    width: 200,
-                    marginLeft: 50,
-                    color: '#F5E8DA',
-                    textTransform: 'upperCase',
-                  },
-                  input: {
-                    padding: 5,
-                    borderColor: '#D9D9D9',
-                    backgroundColor: '#F5E8DA',
-                    borderRadius: 25,
-                    color: 'black',
-                    textAlign: 'centre',
-                    width: 300,
-                  },
-                  label: {
-                    display: 'none',
-                  },
-                  anchor: {
-                    color: '#540375',
-                  },
-                },
-              }}
-            />
-          </>
-        ) : (
-          <></>
-        )}
-      </div>
-    </Layout>
+    <>
+      <TopBar />
+      <Layout home>
+        <div className='flex items-center justify-center font-cursive text-custom'>
+          {!session ? (
+            <>
+              <div>
+                <h1 className='flex justify-center font-cursive text-accentcolor1 text-6xl py-6'>
+                  SIGN IN
+                </h1>
+                <Auth
+                  supabaseClient={supabase}
+                  providers
+                  appearance={{
+                    style: {
+                      button: {
+                        padding: 10,
+                        borderColor: '#540375',
+                        backgroundColor: '#540375',
+                        borderRadius: 7,
+                        width: 200,
+                        marginLeft: 50,
+                        color: '#F5E8DA',
+                        textTransform: 'upperCase',
+                      },
+                      input: {
+                        padding: 5,
+                        borderColor: '#D9D9D9',
+                        backgroundColor: '#F5E8DA',
+                        borderRadius: 25,
+                        color: 'black',
+                        textAlign: 'centre',
+                        width: 300,
+                      },
+                      label: {
+                        display: 'none',
+                      },
+                      anchor: {
+                        color: '#540375',
+                      },
+                    },
+                  }}
+                />
+              </div>
+            </>
+          ) : (
+            <></>
+          )}
+        </div>
+      </Layout>
+    </>
   );
 }
