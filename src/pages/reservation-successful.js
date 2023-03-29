36301ff0e6b71cf58e3cfb1126e593c751eef516
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-29 12:58:00 +0100

    Work on text content

--- src/pages/reservation-successful.js
+++ src/pages/reservation-successful.js
@@ -5,7 +5,9 @@ const ReservationSuccessful = () => {
   return (
     <>
       <Layout pageTitle={pageTitle}>
-        <p>CONGRATULATIONS!</p>
+        <p className='flex justify-center font-cursive text-accentcolor1 text-4xl py-6'>
+          CONGRATULATIONS!
+        </p>
         <p>Your reservation is successful</p>
         <p>Your Pick Up code is:</p>
       </Layout>
