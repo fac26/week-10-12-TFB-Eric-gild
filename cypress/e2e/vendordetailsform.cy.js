44461a3610f27c8d9fc5a8487e9b11490542d784
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-30 16:51:13 +0100

    vendordetailsform testing updated

--- cypress/e2e/vendordetailsform.cy.js
+++ cypress/e2e/vendordetailsform.cy.js
@@ -3,6 +3,7 @@ describe('Vendor details form', () => {
     cy.visit('http://localhost:3000/vendor-upload-form');
     cy.get('input[name="companyname"]').type('Pret');
     cy.get('textarea[name="description"]').type('We sell food');
+    cy.get('input[name="companyhours"]').type('08:00-18:00');
     cy.get('textarea[name="address"]').type('100 High Street N16');
     cy.get('input[name="phonenumber"]').type('01010010101');
     cy.get('form').submit();
