ae1c0ba32c7a871cbd9c074e1514df5333bddfe8
LauraK0 <laurakeating@live.co.uk>
2023-03-30 12:48:57 +0100

    router removed for now

--- src/pages/more-info.js
+++ src/pages/more-info.js
@@ -1,16 +1,13 @@
 import Layout from '@components/Layout';
-import { useMatch } from 'react-router-dom';
+// import { useMatch } from 'react-router-dom';
 
 export default function MoreInfo() {
-  const match = useMatch('/more-info/:id');
-  const itemId = match.params.id;
+  //   const match = useMatch('/more-info/:id');
+  //   const itemId = match.params.id;
 
-  console.log(match);
   return (
     <Layout home>
-      <div className='flex flex-row justify-between gap-6 px-2 py-2'>
-        {JSON.stringify(item)}
-      </div>
+      <div className='flex flex-row justify-between gap-6 px-2 py-2'>hello</div>
     </Layout>
   );
 }
