a09832c8933c35dc3568f207b994d6ed61e4c57c
LauraK0 <laurakeating@live.co.uk>
2023-03-30 10:37:31 +0100

    typo fixed

--- src/components/ButtonQuantity.js
+++ src/components/ButtonQuantity.js
@@ -4,7 +4,7 @@ export default function ButtonSmall(props) {
   const title = props.buttonName || 'placeholder';
   return (
     <button
-      className={`flex items-center justify-center max-w-xs bg-dim-black font-cursive text-accentcolor2 tracking-widest text-xl bg-accentcolor1 py-1 px-4 rounded-lg hover:bg-cream hover:text-accentcolor1 ${
+      className={`flex items-center justify-center max-w-xs bg-dim-black font-cursive text-accentcolor2 tracking-widest text-xl bg-accentcolor1 py-1 px-4 rounded-lg hover:bg-accentcolor3 hover:text-accentcolor1 ${
         props.quantity === 0 &&
         props.buttonName === '-' &&
         'opacity-50 cursor-not-allowed'
