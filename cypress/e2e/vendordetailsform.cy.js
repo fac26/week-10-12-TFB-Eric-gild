fb1f0ea74355b61588e052441abb1e43c308bd4d
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-03-30 11:24:30 +0100

    Sign and vendor details upload pass

--- cypress/e2e/vendordetailsform.cy.js
+++ cypress/e2e/vendordetailsform.cy.js
@@ -1,7 +1,10 @@
-cy.get('form').within(($form) => {
-  // cy.get() will only search for elements within form,
-  // not within the entire document
-  cy.get('input[name="email"]').type('john.doe@email.com');
-  cy.get('input[name="password"]').type('password');
-  cy.root().submit();
+describe('Vendor details form', () => {
+  it('Submitted vendor details successfully', () => {
+    cy.visit('http://localhost:3000/vendor-upload-form');
+    cy.get('input[name="companyname"]').type('Pret');
+    cy.get('textarea[name="description"]').type('We sell food');
+    cy.get('textarea[name="address"]').type('100 High Street N16');
+    cy.get('input[name="phonenumber"]').type('01010010101');
+    cy.get('form').submit();
+  });
 });
