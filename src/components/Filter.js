b2f0d9e72b755a0e62a21691f945ac25688d8903
LauraK0 <laurakeating@live.co.uk>
2023-03-31 16:31:28 +0100

    styling added

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -10,23 +10,28 @@ export default function Filter(props) {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit((data) => {
-        setFoodFilter(data);
-      })}
-    >
-      <select {...register('Filter')}>
-        <option value='All'>All</option>
-        {filters
-          ? filters.map((filter) => (
-              <option value={filter.Name} key={filter.ID}>
-                {filter.Name}
-              </option>
-            ))
-          : null}
-        <option value='Test'>Test</option>
-      </select>
-      <Button buttonName={'Filter'} ButtonOnClick={handleSubmit(onSubmit)} />
-    </form>
+    <div className='flex flex-col items-center'>
+      <form
+        onSubmit={handleSubmit((data) => {
+          setFoodFilter(data);
+        })}
+      >
+        <select
+          {...register('Filter')}
+          className='bg-accentcolor3 text-gray-900 text-sm rounded-md block w-full font-sans h-8 mb-3'
+        >
+          <option value='All'>All</option>
+          {filters
+            ? filters.map((filter) => (
+                <option value={filter.Name} key={filter.ID}>
+                  {filter.Name}
+                </option>
+              ))
+            : null}
+          <option value='Test'>Test</option>
+        </select>
+        <Button buttonName={'Filter'} ButtonOnClick={handleSubmit(onSubmit)} />
+      </form>
+    </div>
   );
 }
