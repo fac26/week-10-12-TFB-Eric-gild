767a89673647309e823c65e363341698a60d68ae
ighellache <78818760+ighellache@users.noreply.github.com>
2023-03-31 12:25:04 +0100

    more-info page rendered with modal

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,15 +10,19 @@ export default function Card(props) {
   const handleMoreInfoClick = () => {
     router.push({
       pathname: '/test',
-      query: { item: JSON.stringify(item) },
+      query: {
+        item: JSON.stringify(item),
+        collaborator: JSON.stringify(collaborator),
+      },
     });
-    console.log(item);
   };
 
   return (
     <div className='w-11/12 mx-auto max-w-screen-sm text-accentcolor1 tracking-widest bg-accentcolor2 font-cursive py-6 px-6 rounded-lg'>
       <div className='mx-auto max-w-md'>
-        <h3 className='font-cursive text-5xl'>{foodItemName.toUpperCase()}</h3>
+        <h3 className='font-cursive text-5xl'>
+          {item && item.name && item.name.toUpperCase()}
+        </h3>
         <div className='flex flex-row gap-6 w-5/6 h-5/6'>
           <div className=' flex items-center justify-center'>
             <img
--- src/pages/test.js
+++ src/pages/test.js
@@ -1,36 +1,33 @@
 import Layout from 'components/Layout';
 import { useRouter } from 'next/router';
 import Modal from 'react-modal';
-import Button from 'components/Button';
 import { useState } from 'react';
 
 export default function MoreInfo() {
   const router = useRouter();
-  const { item } = router.query;
-  console.log(item);
-  const { name, description, image, allergens, menuName } = JSON.parse(item);
+  const { item, collaborator } = router.query;
+  const { name, description, image, allergens } = JSON.parse(item);
+  const { Name, Address, Hours, PhoneNumber } = JSON.parse(collaborator);
   const [modalOpen, setModalOpen] = useState(false);
 
   return (
     <Layout>
-      <br></br>
-      <div className='bg-white flex flex-col items-center justify-center gap-2 w-full'>
-        <img src={image[0].url} alt={name} width={200} height={200} />
-
+      <div className='bg-white flex flex-col items-center justify-center gap-2 w-full h-screen'>
+        <div className='h-1/2 w-screen flex items-center justify-center'>
+          <img src={image[0].url} alt={name} className='h-full' />
+        </div>
         <h1 className='font-cursive text-accentcolor1 text-5xl'>{name}</h1>
         <p className='flex justify-center text-center text-accentcolor1 text-sm'>
           {description}
         </p>
         <div className='text-accentcolor1 text-center text-sm mb-40'>
-          <p>{menuName === 'pret' ? 'Pret a Manger' : menuName}</p>
-          {/* should show table name */}
-          <p>271 Holloway Road, London N7, UK</p>
-          {/* show address from collaborators */}
-          <p>Collect: </p>
+          <p>{Name}</p>
+          <p>{Address}</p>
+          <p>Collect: {Hours}</p>
+          <p>Contact: {PhoneNumber}</p>
           <button onClick={setModalOpen}>
             Click here for ingredients and allergens
           </button>
-          {/* open a modal */}
           <Modal isOpen={modalOpen} onRequestClose={() => setModalOpen(false)}>
             <div>{allergens}</div>
             <div className='flex justify-end mt-4'>
@@ -38,7 +35,7 @@ export default function MoreInfo() {
                 className='flex items-center justify-center max-w-xs w-40 bg-dim-black font-cursive text-accentcolor3 tracking-widest text-3xl bg-accentcolor1 py-2 px-2 rounded-lg hover:bg-green hover:text-accentcolor1'
                 onClick={() => setModalOpen(false)}
               >
-                Close Modal
+                Close
               </button>
             </div>
           </Modal>
