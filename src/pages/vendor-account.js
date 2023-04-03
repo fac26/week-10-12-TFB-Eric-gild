f27df587b1e97ea5c1cb44788abf265f2450a741
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-04-03 17:35:51 +0100

    New attempt at retrieving Airtable data

--- src/pages/vendor-account.js
+++ src/pages/vendor-account.js
@@ -32,7 +32,7 @@ export default function VendorDetails({ collaborators }) {
 
   return (
     <Layout pageTitle={pageTitle} isBusinessPage>
-      <div>{JSON.stringify(collaborator?.Name)}</div>
+      <div>{collaborator?.Name}</div>;
       {/* <div className='flex flex-col m-4 items-center'>
         <p className='text-accentcolor2 text-center font-sans text-lg leading-1.5 m-0 max-w-30rem px-6 mx-auto'>
           {nameFields.name}
