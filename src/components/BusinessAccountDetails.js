03dbe9a29ac9c5e1ead3671fa0d9a13d8794736f
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-04-03 12:32:38 +0100

    Resized fonts

--- src/components/BusinessAccountDetails.js
+++ src/components/BusinessAccountDetails.js
@@ -4,7 +4,7 @@ export default function BusinessAccountDetails(props) {
   return (
     <div className='w-11/12 mx-auto max-w-screen-sm text-accentcolor1 tracking-widest bg-accentcolor2 font-cursive py-6 px-6 rounded-lg'>
       <h3 className='text-4xl'>Description</h3>
-      <p className='flex justify-center text-center text-accentcolor1 text-md'>
+      <p className='flex justify-start text-center text-accentcolor1 text-2xl'>
         Pret a Manger is a sandwich shop serving soup, sandwiches, hot drinks,
         and more.
       </p>
@@ -12,14 +12,14 @@ export default function BusinessAccountDetails(props) {
         <ButtonSmall buttonName={'Edit'} buttonLink='/' />
       </div>
       <h3 className='text-4xl'>Address</h3>
-      <p className='flex justify-center text-center text-accentcolor1 text-md'>
+      <div className='flex justify-start text-center text-accentcolor1 text-2xl'>
         271 Holloway Road, London, N7 8JL
-      </p>
+      </div>
       <div className='flex flex-col mb-10 mt-10 items-center'>
         <ButtonSmall buttonName={'Edit'} buttonLink='/' />
       </div>
       <h3 className='text-4xl'>Frequently Asked Qs</h3>
-      <p className='flex justify-center text-center text-accentcolor1 text-md'>
+      <p className='flex justify-start text-center text-accentcolor1 text-2xl'>
         Description
       </p>
       <div className='flex flex-col mb-10 mt-10 items-center'>
