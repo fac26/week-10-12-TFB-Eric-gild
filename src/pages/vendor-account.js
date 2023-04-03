eeb30a6f259347e6f4064fe1cb4cef25a9a08d72
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-04-03 20:33:57 +0100

    Latest attempt at Airtable data

--- src/pages/vendor-account.js
+++ src/pages/vendor-account.js
@@ -26,13 +26,25 @@ export default function VendorDetails({ collaborators }) {
   const pageTitle = 'Vendor account';
   const session = useSession();
   const supabase = useSupabaseClient();
-  const collaborator = collaborators.find(
-    (collaborator) => collaborator.Name === session.user.name
-  );
+  const [collaborator, setCollaborator] = useState(null);
+  useEffect(() => {
+    if (collaborators && session && session.user) {
+      const foundCollaborator = collaborators.find(
+        (collaborator) => collaborator.Name === session.user.name
+      );
+      setCollaborator(foundCollaborator || {});
+    }
+  }, [collaborators, session]);
 
   return (
     <Layout pageTitle={pageTitle} isBusinessPage>
-      <div>{collaborator}</div>;
+      {collaborator && collaborator.Name ? (
+        <div>
+          <p>Collaborator: {collaborator.Name}</p>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
       {/* <div className='flex flex-col m-4 items-center'>
         <p className='text-accentcolor2 text-center font-sans text-lg leading-1.5 m-0 max-w-30rem px-6 mx-auto'>
           {nameFields.name}
