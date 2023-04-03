3099e531c4e7f7c6df585efc3f50ce6bd3dd6b56
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-04-03 14:49:25 +0100

    Git conflict resolve

--- src/components/BusinessAccountDetails.js
+++ src/components/BusinessAccountDetails.js
@@ -7,14 +7,13 @@ import { useEffect } from 'react';
 import airtableModule from 'utils/airtable';
 
 export default function BusinessAccountDetails(props) {
-  const { Description, Address } = collaborator ? JSON.parse(collaborator) : {};
+  // const { Description, Address } = collaborator ? JSON.parse(collaborator) : {};
   return (
     <div className='w-11/12 mx-auto max-w-screen-sm text-accentcolor1 tracking-widest bg-accentcolor2 font-cursive py-6 px-6 rounded-lg'>
       <h3 className='text-4xl'>Description</h3>
       <p className='text-l font-sans overflow-hidden'>
         Pret a Manger is a sandwich shop serving soup, sandwiches, hot drinks,
         and more.
-        {Description}
       </p>
       <div className='flex flex-col mb-10 mt-10 items-center'>
         <div className='ml-auto'>
@@ -24,7 +23,6 @@ export default function BusinessAccountDetails(props) {
       <h3 className='text-4xl'>Address</h3>
       <p className='text-l font-sans overflow-hidden'>
         271 Holloway Road, London, N7 8JL
-        {Address}
       </p>
       <div className='flex flex-col mb-10 mt-10 items-center'>
         <div className='ml-auto'>
