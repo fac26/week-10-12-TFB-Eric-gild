c50ff6a31d40a705ba4036a81dfbac800d65cd9f
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-04-03 15:45:22 +0100

    Retrieving data from Airtable

--- src/pages/vendor-account.js
+++ src/pages/vendor-account.js
@@ -4,43 +4,47 @@ import { useState, useEffect } from 'react';
 import BusinessAccountDetails from 'components/BusinessAccountDetails';
 
 export async function getServerSideProps() {
-  const name = await airtableModule.getRecords('Collaborators');
-  if (!name) {
+  const collaborator =
+    '{ "Name": "Pret", "Address": "271 Holloway Road, London N7 8JL" }'; // Replace with actual collaborator data
+  const { Name, Address } = JSON.parse(collaborator);
+  const name = await airtableModule.getRecords({
+    filterByFormula: `{Name} = '${Name}'`,
+  });
+  const address = await airtableModule.getRecords({
+    filterByFormula: `{Address} = '${Address}'`,
+  });
+
+  if (!name || name.length === 0 || !address || address.length === 0) {
     return {
       notFound: true,
     };
   }
-  if (name) {
-    return {
-      props: {
-        name,
-      },
-    };
-  }
+  return {
+    props: {
+      name: name[0],
+      address: address[0],
+    },
+  };
 }
 
-export default function VendorAccountDetails({ name }) {
-  const [items, setItems] = useState([]);
+export default function VendorAccountDetails({ name, address }) {
+  const [nameFields, setNameFields] = useState({});
+  const [addressFields, setAddressFields] = useState({});
   const pageTitle = 'Business Account Page';
 
   useEffect(() => {
-    const newItems = name.map((item) => ({
-      id: item.ID,
-      fields: {
-        name: item.name,
-      },
-    }));
-    setItems(newItems);
-  }, [name]);
+    setNameFields(name.fields);
+    setAddressFields(address.fields);
+  }, [name, address]);
 
   return (
     <Layout pageTitle={pageTitle} isBusinessPage>
       <div className='flex flex-col m-4 items-center'>
         <p className='text-accentcolor2 text-center font-sans text-lg leading-1.5 m-0 max-w-30rem px-6 mx-auto'>
-          [name]
+          {nameFields.name}
         </p>
         <p className='text-accentcolor2 text-center font-sans text-lg leading-1.5 m-0 max-w-30rem px-6 mx-auto'>
-          271 Holloway Road, London N7 8JL
+          {addressFields.name}
         </p>
       </div>
       <div className='flex flex-col m-4 items-center gap-4'></div>
