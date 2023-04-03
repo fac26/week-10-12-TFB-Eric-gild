2c6899bcb0e6aed1ccacaafd68e76a7f0f3c35e3
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-04-03 16:23:25 +0100

    Attempting to fetch from Airtable for Name and Address

--- src/pages/vendor-account.js
+++ src/pages/vendor-account.js
@@ -3,52 +3,49 @@ import airtableModule from 'utils/airtable';
 import { useState, useEffect } from 'react';
 import BusinessAccountDetails from 'components/BusinessAccountDetails';
 
-export async function getServerSideProps() {
-  const collaborator =
-    '{ "Name": "Pret", "Address": "271 Holloway Road, London N7 8JL" }'; // Replace with actual collaborator data
-  const { Name, Address } = JSON.parse(collaborator);
-  const name = await airtableModule.getRecords({
-    filterByFormula: `{Name} = '${Name}'`,
-  });
-  const address = await airtableModule.getRecords({
-    filterByFormula: `{Address} = '${Address}'`,
-  });
+var Airtable = require('airtable');
+var base = new Airtable({
+  AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
+}).base('appwjTMNZwwnhuRzz');
 
-  if (!name || name.length === 0 || !address || address.length === 0) {
-    return {
-      notFound: true,
-    };
-  }
-  return {
-    props: {
-      name: name[0],
-      address: address[0],
+base('Collaborators')
+  .select({
+    // Selecting the first 3 records in FoodVendors:
+    maxRecords: 3,
+    view: 'FoodVendors',
+  })
+  .eachPage(
+    function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+
+      records.forEach(function (record) {
+        console.log('Retrieved', record.get('Name'));
+      });
+
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
     },
-  };
-}
-
-export default function VendorAccountDetails({ name, address }) {
-  const [nameFields, setNameFields] = useState({});
-  const [addressFields, setAddressFields] = useState({});
-  const pageTitle = 'Business Account Page';
-
-  useEffect(() => {
-    setNameFields(name.fields);
-    setAddressFields(address.fields);
-  }, [name, address]);
-
-  return (
-    <Layout pageTitle={pageTitle} isBusinessPage>
-      <div className='flex flex-col m-4 items-center'>
-        <p className='text-accentcolor2 text-center font-sans text-lg leading-1.5 m-0 max-w-30rem px-6 mx-auto'>
-          {nameFields.name}
-        </p>
-        <p className='text-accentcolor2 text-center font-sans text-lg leading-1.5 m-0 max-w-30rem px-6 mx-auto'>
-          {addressFields.name}
-        </p>
-      </div>
-      <div className='flex flex-col m-4 items-center gap-4'></div>
-      <BusinessAccountDetails />
-    </Layout>
+    function done(err) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    }
   );
-}
+
+// return (
+//   <Layout pageTitle={pageTitle} isBusinessPage>
+//     <div className='flex flex-col m-4 items-center'>
+//       <p className='text-accentcolor2 text-center font-sans text-lg leading-1.5 m-0 max-w-30rem px-6 mx-auto'>
+//         {nameFields.name}
+//       </p>
+//       <p className='text-accentcolor2 text-center font-sans text-lg leading-1.5 m-0 max-w-30rem px-6 mx-auto'>
+//         {addressFields.name}
+//       </p>
+//     </div>
+//     <div className='flex flex-col m-4 items-center gap-4'></div>
+//     <BusinessAccountDetails />
+//   </Layout>
+// );
