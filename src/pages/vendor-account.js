5a319e705a6070c9f07ac650553d46c0547c78a2
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-04-03 15:29:05 +0100

    Added name query from Airtable

--- src/pages/vendor-account.js
+++ src/pages/vendor-account.js
@@ -4,41 +4,40 @@ import { useState, useEffect } from 'react';
 import BusinessAccountDetails from 'components/BusinessAccountDetails';
 
 export async function getServerSideProps() {
-  const description = await airtableModule.getRecords('Collaborators');
-  if (!description) {
+  const name = await airtableModule.getRecords('Collaborators');
+  if (!name) {
     return {
       notFound: true,
     };
   }
-  if (description) {
+  if (name) {
     return {
       props: {
-        description,
+        name,
       },
     };
   }
 }
 
-export default function VendorAccountDetails({ description }) {
+export default function VendorAccountDetails({ name }) {
   const [items, setItems] = useState([]);
   const pageTitle = 'Business Account Page';
 
   useEffect(() => {
-    const newItems = description.map((item) => ({
+    const newItems = name.map((item) => ({
       id: item.ID,
       fields: {
         name: item.name,
-        quantity: item.quantity,
       },
     }));
     setItems(newItems);
-  }, [description]);
+  }, [name]);
 
   return (
     <Layout pageTitle={pageTitle} isBusinessPage>
       <div className='flex flex-col m-4 items-center'>
         <p className='text-accentcolor2 text-center font-sans text-lg leading-1.5 m-0 max-w-30rem px-6 mx-auto'>
-          Pret
+          [name]
         </p>
         <p className='text-accentcolor2 text-center font-sans text-lg leading-1.5 m-0 max-w-30rem px-6 mx-auto'>
           271 Holloway Road, London N7 8JL
