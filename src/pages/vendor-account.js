6451b4a6f1f82f8308836ee1360586a6bddad273
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-04-04 12:32:59 +0100

    Formatted and styled name and address fields

--- src/pages/vendor-account.js
+++ src/pages/vendor-account.js
@@ -2,8 +2,7 @@ import Layout from 'components/Layout';
 import airtableModule from 'utils/airtable';
 import { useState, useEffect } from 'react';
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
-
-// import BusinessAccountDetails from 'components/BusinessAccountDetails';
+import BusinessAccountDetails from 'components/BusinessAccountDetails';
 import React from 'react';
 
 export async function getServerSideProps() {
@@ -36,20 +35,16 @@ export default function VendorDetails({ collaborators }) {
 
   return (
     <Layout pageTitle={pageTitle} isBusinessPage>
-      <div>
-        <p>{collaborator.Name}</p>
-        <p>{collaborator.Address}</p>
+      <div className='flex flex-col m-4 items-center'>
+        <p className='text-accentcolor2 text-center font-sans text-lg leading-1.5 m-0 max-w-30rem px-6 mx-auto'>
+          {collaborator.Name}
+        </p>
+        <p className='text-accentcolor2 text-center font-sans text-lg leading-1.5 m-0 max-w-30rem px-6 mx-auto'>
+          {collaborator.Address}
+        </p>
       </div>
-      {/* <div className='flex flex-col m-4 items-center'>
-          <p className='text-accentcolor2 text-center font-sans text-lg leading-1.5 m-0 max-w-30rem px-6 mx-auto'>
-            {nameFields.name}
-          </p>
-          <p className='text-accentcolor2 text-center font-sans text-lg leading-1.5 m-0 max-w-30rem px-6 mx-auto'>
-            {addressFields.name}
-          </p>
-        </div>
-        <div className='flex flex-col m-4 items-center gap-4'></div>
-        <BusinessAccountDetails /> */}
+      <div className='flex flex-col m-4 items-center gap-4'></div>
+      <BusinessAccountDetails />
     </Layout>
   );
 }
