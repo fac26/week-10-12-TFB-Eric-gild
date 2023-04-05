b550382be2c3eebabb9b0e77f6d27c09978d1791
LauraK0 <laurakeating@live.co.uk>
2023-04-05 12:03:26 +0100

    signIn styling updated

--- src/pages/sign-in-user.js
+++ src/pages/sign-in-user.js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import Layout from 'components/Layout';
 import TopBar from 'components/TopBar';
+import { signIn, cards } from '@styles/index.js';
 
 export default function SignIn() {
   const session = useSession();
@@ -18,56 +19,30 @@ export default function SignIn() {
 
   return (
     <>
-      <TopBar />
-      <br />
-      <br />
-      <br />
-      <Layout home>
-        <div className='flex items-center justify-center font-cursive text-custom'>
+      <Layout Auth pageTitle='Sign In'>
+        <div className='mx-auto max-w-md items-center text-accentcolor3'>
           {!session ? (
             <>
               <div>
-                <h1>Please make an account to reserve your meal!</h1>
-                <h1 className='flex justify-center font-cursive text-accentcolor1 text-6xl py-6'>
-                  SIGN IN
+                <h1 className={cards.normalTextCenter}>
+                  Please make an account to reserve your meal!
                 </h1>
                 <Auth
                   supabaseClient={supabase}
                   providers
-                  appearance={{
-                    style: {
-                      button: {
-                        padding: 10,
-                        borderColor: '#540375',
-                        backgroundColor: '#540375',
-                        borderRadius: 7,
-                        width: 200,
-                        marginLeft: 50,
-                        color: '#F5E8DA',
-                        textTransform: 'upperCase',
-                      },
-                      input: {
-                        padding: 5,
-                        borderColor: '#D9D9D9',
-                        backgroundColor: '#F5E8DA',
-                        borderRadius: 25,
-                        color: 'black',
-                        textAlign: 'centre',
-                        width: 300,
-                      },
-                      label: {
-                        display: 'none',
-                      },
-                      anchor: {
-                        color: '#540375',
-                      },
-                    },
-                  }}
+                  appearance={signIn.supabase}
                 />
               </div>
             </>
           ) : (
-            <></>
+            <>
+              <p className={cards.normalTextCenter}>
+                Sorry, you are currently signed.
+              </p>
+              <p className={cards.normalTextCenter}>
+                Please sign-out before signing in.
+              </p>
+            </>
           )}
         </div>
       </Layout>
